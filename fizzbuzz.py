a7cea31413f9210540e8e7f4226e94c02d0a06c7
Kingsley Ihemere <dekings44@gmail.com>
2022-05-23 00:06:20 +0100

    fizzbuzz challenge

---
+++ fizzbuzz.py
@@ -0,0 +1,10 @@
+print('Welcome to average student height calculator')
+
+
+for num in range(1, 100):#
+    if num % 3 == 0 and num % 5 != 0:
+        print(f'{num} Fizz')
+    elif num % 5 == 0 and num % 3 != 0:
+        print(f'{num} Buzz')
+    elif num % 3 == 0 or num % 5 == 0:
+        print(f'{num} FizzBuzz')
